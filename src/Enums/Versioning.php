c53cf91dd81ed40e6957b0206fcaf36666c98a33
Lukasz Lupa <lukasz.lupa@gmail.com>
2023-08-26 00:45:19 +0200

    Create a function to add version into uri

---
+++ src/Enums/Versioning.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Twirelab\LaravelRouter\Enums;
+
+enum Versioning: string
+{
+    case NEUTRAL = 'NEUTRAL';
+}
--- src/Traits/Loader.php
+++ src/Traits/Loader.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Twirelab\LaravelRouter\Traits;
 
 use Illuminate\Routing\Router as LaravelRouter;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use InvalidArgumentException;
@@ -13,6 +15,7 @@ use ReflectionClass;
 use ReflectionMethod;
 use Twirelab\LaravelRouter\Annotations\Method;
 use Twirelab\LaravelRouter\Annotations\Router;
+use Twirelab\LaravelRouter\Enums\Versioning;
 
 trait Loader
 {
@@ -36,23 +39,32 @@ trait Loader
 
         $controller = $this->getController($class);
 
-        Route::group(
-            $controller,
-            fn (LaravelRouter $router) => $this->loadMethods(
-                router: $router,
-                methods: $class->getMethods(),
-                data: $controller,
-                class: $class
-            )
+        if(!in_array(
+            needle: $controller['version'],
+            haystack: Arr::wrap(Config::get('router.acceptable_versions'))
+        )) {
+            return;
+        }
+
+        $this->loadMethods(
+            methods: $class->getMethods(),
+            controller: $controller,
+            class: $class
         );
     }
 
     /**
      * Set a controller data.
      */
-    private function setControllerData(string $name = null, string $prefix = null, string $domain = null, string|array $middlewares = null): array
+    private function setControllerData(
+        string $name = null,
+        string $prefix = null,
+        string $domain = null,
+        string|array $middlewares = null,
+        string $version = null
+    ): array
     {
-        return compact('name', 'prefix', 'domain', 'middlewares');
+        return compact('name', 'prefix', 'domain', 'middlewares', 'version');
     }
 
     /**
@@ -72,7 +84,8 @@ trait Loader
                 name: $annotation->getName(),
                 prefix: $annotation->getPrefix(),
                 domain: $annotation->getDomain(),
-                middlewares: $annotation->getMiddlewares()
+                middlewares: $annotation->getMiddlewares(),
+                version: $annotation->getVersion()
             );
         }
 
@@ -82,14 +95,20 @@ trait Loader
     /**
      * Load methods from a controller.
      */
-    private function loadMethods(LaravelRouter $router, array $methods, array $data, ReflectionClass $class): void
+    private function loadMethods(array $methods, array $controller, ReflectionClass $class): void
     {
         foreach ($methods as $method) {
             foreach ($this->loadAnnotationMethods($method) as $annotation) {
+                if(in_array(
+                    needle: $annotation->getVersion(),
+                    haystack: Arr::wrap(Config::get('router.acceptable_versions'))
+                )) {
+                    continue;
+                }
+
                 $this->addRoute(
-                    router: $router,
                     annotation: $annotation,
-                    data: $data,
+                    controller: $controller,
                     class: $class,
                     method: $method
                 );
@@ -110,17 +129,24 @@ trait Loader
     /**
      * Add route.
      */
-    private function addRoute(LaravelRouter $router, Method $annotation, array $data, ReflectionClass $class, ReflectionMethod $method): void
+    private function addRoute(Method $annotation, array $controller, ReflectionClass $class, ReflectionMethod $method): void
     {
-        $name = $data['name'].($annotation->getName() ?? Str::snake($method->getName()));
+        $methodVersion = $annotation->getVersion() === Versioning::NEUTRAL->value ? null : $annotation->getVersion();
+        $controllerVersion = $controller['version'] === Versioning::NEUTRAL->value ? null : $annotation['version'];
+        $version = $methodVersion ?: $controllerVersion ?: null;
+        $uri = $version . $annotation->getUri();
 
-        $router
-            ->{$annotation->getMethod()}($annotation->getUri(), [$class->getName(), $method->getName()])
+        $name = ($version ? Str::snake($version) . '.' : null) .$controller['name'].($annotation->getName() ?? Str::snake($method->getName()));
+
+        $route = Route::{$annotation->getMethod()}($uri, [$class->getName(), $method->getName()])
             ->name($name)
-            ->middleware($annotation->getMiddlewares());
+            ->middleware([
+                ...$controller['middlewares'],
+                ...$annotation->getMiddlewares(),
+            ]);
 
         if ($annotation->getWhere()) {
-            $router->where($annotation->getWhere());
+            $route->where($annotation->getWhere());
         }
     }
 }
